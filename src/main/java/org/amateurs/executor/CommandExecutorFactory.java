43ba64cd009e720f1fe19f2867f25d81167cf2c5
Joanne Ong <joanneongcf@u.nus.edu>
2025-02-25 23:46:29 +0800

    Fix missing return statement for CommandExecutorFactory

--- src/main/java/org/amateurs/executor/CommandExecutorFactory.java
+++ src/main/java/org/amateurs/executor/CommandExecutorFactory.java
@@ -4,10 +4,12 @@ import org.amateurs.Command;
 
 public class CommandExecutorFactory {
     public static CommandExecutor getCommandExecutor(Command command) {
-        switch (command) {
+        return switch (command) {
             case START_COMMAND -> new StartCommandExecutor();
             case LIST_COMMAND -> new ListCommandExecutor();
             case ADD_COMMAND -> new AddCommandExecutor();
-        }
+            case EDIT_COMMAND -> new EditCommandExecutor();
+            case DELETE_COMMAND -> new DeleteCommandExecutor();
+        };
     }
 }
---
+++ src/main/java/org/amateurs/executor/EditCommandExecutor.java
@@ -0,0 +1,13 @@
+package org.amateurs.executor;
+
+public class EditCommandExecutor implements CommandExecutor {
+    @Override
+    public void executeCommand(Long chatId) {
+
+    }
+
+    @Override
+    public void executeCallbackQuery(Long chatId, int msgId, String queryId, String callbackData) {
+
+    }
+}
