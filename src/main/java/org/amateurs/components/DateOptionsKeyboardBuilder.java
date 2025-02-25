22901c09a448b1fbf083ded860600976775535bd
Joanne Ong <joanneongcf@u.nus.edu>
2025-02-26 00:05:22 +0800

    Refactor

---
+++ src/main/java/org/amateurs/components/DateOptionsKeyboardBuilder.java
@@ -0,0 +1,55 @@
+package org.amateurs.components;
+
+import org.amateurs.Command;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardRow;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DateOptionsKeyboardBuilder {
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd LLL yyyy (EEE)");
+
+    public static InlineKeyboardMarkup buildDateOptionsKeyboard(int numDays, int buttonsPerRow, String callbackPrefix) {
+        final ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Asia/Kuala_Lumpur"));
+
+        int numRows = Math.ceilDiv(numDays, buttonsPerRow);
+
+        final List<List<InlineKeyboardButton>> dateButtons = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            dateButtons.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < numDays; i++) {
+            final List<InlineKeyboardButton> dateRow = dateButtons.get(i / buttonsPerRow);
+            final InlineKeyboardButton dateButton = buildDateButton(now.plusDays(i + 1), callbackPrefix);
+            dateRow.add(dateButton);
+        }
+
+        final List<InlineKeyboardRow> dateRows = dateButtons.stream()
+                .map(InlineKeyboardRow::new)
+                .toList();
+
+        return InlineKeyboardMarkup.builder()
+                .keyboard(dateRows)
+                .build();
+    }
+
+    /**
+     * Returns a date button corresponding to an input date
+     * @param date              input date to create button for
+     * @param callbackPrefix    prefix to use for button callback data
+     * @return                  date button corresponding to input date
+     */
+    public static InlineKeyboardButton buildDateButton(ZonedDateTime date, String callbackPrefix) {
+        final String formattedDate = DATE_FORMATTER.format(date);
+        return InlineKeyboardButton.builder()
+                .text(formattedDate)
+                .callbackData(String.join(Command.COMMAND_DELIMITER, callbackPrefix, formattedDate))
+                .build();
+    }
+}
--- src/main/java/org/amateurs/executor/AddCommandExecutor.java
+++ src/main/java/org/amateurs/executor/AddCommandExecutor.java
@@ -2,16 +2,9 @@ package org.amateurs.executor;
 
 import org.amateurs.ChatClient;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardRow;
-
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.amateurs.Command.ADD_COMMAND;
+import static org.amateurs.components.DateOptionsKeyboardBuilder.buildDateOptionsKeyboard;
 
 public class AddCommandExecutor implements CommandExecutor {
     private static final String ADD_TEMPLATE = """
@@ -19,8 +12,6 @@ public class AddCommandExecutor implements CommandExecutor {
             
             When is the game going to be?
             """;
-
-    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd LLL yyyy (EEE)");
 
     private final ChatClient chatClient;
 
@@ -30,44 +21,13 @@ public class AddCommandExecutor implements CommandExecutor {
 
     @Override
     public void executeCommand(Long chatId) {
-        chatClient.sendMenu(chatId, ADD_TEMPLATE, buildDateOptionsKeyboard());
+        final InlineKeyboardMarkup dateKeyboard = buildDateOptionsKeyboard(15, 5, ADD_COMMAND.getCommand());
+        chatClient.sendMenu(chatId, ADD_TEMPLATE, dateKeyboard);
     }
 
     @Override
     public void executeCallbackQuery(Long chatId, int msgId, String queryId, String callbackData) {
         chatClient.closeCallbackQuery(queryId);
         executeCommand(chatId);
-    }
-
-    private InlineKeyboardMarkup buildDateOptionsKeyboard() {
-        final ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Asia/Kuala_Lumpur"));
-
-        // 14 options in total. There are 3 rows, each row has 5 options
-        final List<List<InlineKeyboardButton>> dateButtons = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            dateButtons.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < 15; i++) {
-            final List<InlineKeyboardButton> dateRow = dateButtons.get(i / 5);
-            final InlineKeyboardButton dateButton = buildDateButton(now.plusDays(i + 1));
-            dateRow.add(dateButton);
-        }
-
-        final List<InlineKeyboardRow> dateRows = dateButtons.stream()
-                .map(InlineKeyboardRow::new)
-                .toList();
-
-        return InlineKeyboardMarkup.builder()
-                .keyboard(dateRows)
-                .build();
-    }
-
-    private InlineKeyboardButton buildDateButton(ZonedDateTime date) {
-        final String formattedDate = DATE_FORMATTER.format(date);
-        return InlineKeyboardButton.builder()
-                .text(formattedDate)
-                .callbackData(ADD_COMMAND + ":DATE:" + formattedDate)
-                .build();
     }
 }
